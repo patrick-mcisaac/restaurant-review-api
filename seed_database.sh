3c2db1647f352a844ea382164f8429bc8c03458a
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-12-04 11:45:05 -0600

    Initial commit

---
+++ seed_database.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm db.sqlite3
+rm -rf ./restaurantapi/migrations
+python3 manage.py migrate
+python3 manage.py makemigrations restaurantapi
+python3 manage.py migrate restaurantapi
+python3 manage.py loaddata users
+python3 manage.py loaddata tokens
+
