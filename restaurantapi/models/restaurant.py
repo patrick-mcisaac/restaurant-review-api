6cdc795d3c32b2abe5eba8c5b28f117d51c612ba
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-12-16 11:27:06 -0600

    filtering based off name and location

--- restaurantapi/models/restaurant.py
+++ restaurantapi/models/restaurant.py
@@ -1,17 +1,24 @@
-""" models for restaurants """
+"""models for restaurants"""
 
 from django.db import models
-from django.db.models import  Avg
+from django.db.models import Avg
 from .review import Review
+
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
-    description=models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
     # TODO: make a seperate image model so i can have many images
-    image = models.ImageField(upload_to='images', width_field=None, height_field=None, blank=True, null=True)
+    image = models.ImageField(
+        upload_to="images", width_field=None, height_field=None, blank=True, null=True
+    )
 
     @property
     def average_ratings(self):
-        avg_score = Review.objects.filter(restaurant_location__restaurant=self).aggregate(Avg('score'))['score__avg'] or 0
-        return round(avg_score,2)
-    
+        avg_score = (
+            Review.objects.filter(restaurant_location__restaurant=self).aggregate(
+                Avg("score")
+            )["score__avg"]
+            or 0
+        )
+        return round(avg_score, 2)
--- restaurantapi/views/restaurants.py
+++ restaurantapi/views/restaurants.py
@@ -1,42 +1,56 @@
 from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
-from restaurantapi.models import Restaurant, Review, RestaurantLocation, City
+from restaurantapi.models import Restaurant, RestaurantLocation
 from .Cities import CitySerializer
-from .reviews import ReviewSerializer
 
 
 class Restaurants(ViewSet):
     def list(self, request):
+        search = request.query_params.get("search", None)
+        location = request.query_params.get("locations", None)
         restaurants = Restaurant.objects.all()
-        ser = RestaurantSerializer(restaurants, many=True, context={'restaurants': restaurants, 'user': request.auth.user})
+        if search is not None and search != " ":
+            restaurants = restaurants.filter(name__contains=search)
+        if location is not None and int(location) > 0:
+            restaurants = restaurants.filter(locations__city__id=location)
+        ser = RestaurantSerializer(
+            restaurants,
+            many=True,
+            context={"restaurants": restaurants, "user": request.auth.user},
+        )
         return Response(ser.data, status=status.HTTP_200_OK)
 
-    def retrieve(self,request,pk = None):
+    def retrieve(self, request, pk=None):
         try:
             restaurant = Restaurant.objects.get(pk=pk)
-            ser = RestaurantSerializer(restaurant, many=False, context={'restaurant': restaurant, 'user': request.auth.user})
+            ser = RestaurantSerializer(
+                restaurant,
+                many=False,
+                context={"restaurant": restaurant, "user": request.auth.user},
+            )
             return Response(ser.data, status=status.HTTP_200_OK)
         except Restaurant.DoesNotExist:
             return Response(None, status=status.HTTP_404_NOT_FOUND)
 
+
 class RestaurantLocationSerializer(serializers.ModelSerializer):
 
-    city=CitySerializer()
+    city = CitySerializer()
 
     class Meta:
         model = RestaurantLocation
-        fields = ['id', 'city', 'hours', 'address', 'location_average_rating']
+        fields = ["id", "city", "hours", "address", "location_average_rating"]
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
     locations = serializers.SerializerMethodField()
 
-    def get_locations(self,obj):
+    def get_locations(self, obj):
         locations = RestaurantLocation.objects.filter(restaurant=obj)
         ser = RestaurantLocationSerializer(locations, many=True)
         return ser.data
 
     class Meta:
         model = Restaurant
-        fields = ['id', 'name', 'description', 'image', 'average_ratings', 'locations']
+        fields = ["id", "name", "description", "image", "average_ratings", "locations"]
