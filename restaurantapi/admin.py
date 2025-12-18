7ca5f86951bc31e3ceb4369c719eff3de19eb85b
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-12-18 15:11:33 -0600

    set up admin site and registered models

--- restaurantapi/admin.py
+++ restaurantapi/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from restaurantapi.models import Restaurant, City, Review, DiningExperience
+
 
 # Register your models here.
+
+
+class RestaurantAdmin(admin.ModelAdmin):
+
+    list_display = [
+        "name",
+    ]
+
+
+class ReviewAdminSerializer(admin.ModelAdmin):
+    list_display = (
+        "user__username",
+        "restaurant_location__restaurant__name",
+        "restaurant_location__city__name",
+        "score",
+    )
+
+
+class DiningExperienceAdmin(admin.ModelAdmin):
+    list_display = ("description",)
+
+
+admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(City, RestaurantAdmin)
+admin.site.register(Review, ReviewAdminSerializer)
+admin.site.register(DiningExperience, DiningExperienceAdmin)
--- restaurantproject/urls.py
+++ restaurantproject/urls.py
@@ -6,17 +6,17 @@ from rest_framework import routers
 from restaurantapi.views import Restaurants, Reviews, Cities, Auth, DiningExperiences
 
 router = routers.DefaultRouter(trailing_slash=False)
-router.register(r'restaurants', Restaurants, 'restaurant')
-router.register(r'cities', Cities, 'city')
-router.register(r'reviews', Reviews, 'review')
-router.register(r'experiences', DiningExperiences, 'experience')
+router.register(r"restaurants", Restaurants, "restaurant")
+router.register(r"cities", Cities, "city")
+router.register(r"reviews", Reviews, "review")
+router.register(r"experiences", DiningExperiences, "experience")
 
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('admin/', admin.site.urls ),
-    path('login', Auth.as_view({'post': 'login'}), name='login'),
-    path('register', Auth.as_view({'post': 'register'}), name='register'),
+    path("", include(router.urls)),
+    path("admin/", admin.site.urls),
+    path("login", Auth.as_view({"post": "login"}), name="login"),
+    path("register", Auth.as_view({"post": "register"}), name="register"),
 ]
 
 if settings.DEBUG:
