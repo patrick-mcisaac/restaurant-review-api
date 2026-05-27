64b1f3f3bf68e86620cc23f22ec736955793aa89
Patmc <patrick.mcisaac.dev@gmail.com>
2026-05-26 20:13:58 -0500

    have pytest working with correct settings

--- seed_database.sh
+++ seed_database.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 rm db.sqlite3
-rm -rf ./restaurantapi/migrations
-python3 manage.py migrate
-python3 manage.py makemigrations restaurantapi
-python3 manage.py migrate restaurantapi
-python3 manage.py loaddata users
-python3 manage.py loaddata tokens
-python3 manage.py loaddata cities
-python3 manage.py loaddata restaurants
-python3 manage.py loaddata restaurant_locations
-python3 manage.py loaddata experiences
-python3 manage.py loaddata reviews
+rm -rf ./restaurantapi/migrations --settings=restaurantproject.settings.base
+python3 manage.py makemigrations restaurantapi --settings=restaurantproject.settings.base
+python3 manage.py migrate --settings=restaurantproject.settings.base
+# python3 manage.py migrate restaurantapi --settings=restaurantproject.settings.base
+python3 manage.py loaddata users --settings=restaurantproject.settings.base
+python3 manage.py loaddata tokens --settings=restaurantproject.settings.base
+python3 manage.py loaddata cities --settings=restaurantproject.settings.base
+python3 manage.py loaddata restaurants --settings=restaurantproject.settings.base
+python3 manage.py loaddata restaurant_locations --settings=restaurantproject.settings.base
+python3 manage.py loaddata experiences --settings=restaurantproject.settings.base
+python3 manage.py loaddata reviews --settings=restaurantproject.settings.base
